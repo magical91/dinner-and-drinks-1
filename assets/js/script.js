4870faf0f84fffbaf6c533416249425bc0d61958
Ansab Sidiki <ansab@sidiki.io>
2022-03-23 16:50:16 -0400

    dinner api call working

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ var dinnerBtnEl = document.querySelector("#dinnerBtn");
 var dinnerCard = function() {
     // format the github api url
     var apiKey = "b992d58c769a431bbb2cf392882f3967"
-    var apiUrl = "https://api.spoonacular.com/recipes/random?number=1&tags=vegetarian,dessert&apiKey=" + apiKey;
+    var apiUrl = "https://api.spoonacular.com/recipes/random?number=1&cuisine=italian,american,japanese,mexican,chinese&apiKey=" + apiKey;
     
 
     // make a request to the url
@@ -18,7 +18,7 @@ var dinnerCard = function() {
         if (response.ok) {
             response.json()
             .then(function(data) {
-                console.log(data.id);
+                apiData(data);
             });
         } else {
             alert('Error');
@@ -34,9 +34,32 @@ var dinnerCard = function() {
 
 dinnerBtnEl.addEventListener("click", dinnerCard);
 
-// var apiData = function (recipes) {
-//     console.log(recipes);
-// };
+var apiData = function (data) {
+    var storageData = data.recipes[0];
+    console.log(storageData);
+
+    dinnerNameEl.innerHTML = ""
+    dinnerImageEl.innerHTML = ""
+    dinnerInstructionsEl.innerHTML = ""
+
+    var dinHeading = document.createElement('a');
+    dinHeading.setAttribute("class", "font-weight-bold text-uppercase");
+    dinHeading.textContent = data.recipes[0].title;
+    dinHeading.href = data.recipes[0].sourceUrl;
+    dinnerNameEl.appendChild(dinHeading);
+    
+
+    var dinImg = document.createElement('img')
+    dinImg.src = data.recipes[0].image
+    dinImg.style.width = "200px";
+    dinImg.style.height = "200px";
+    dinnerImageEl.appendChild(dinImg);
+
+    var dinIns = document.createElement('p');
+    dinIns.textContent = data.recipes[0].instructions;
+    dinnerInstructionsEl.appendChild(dinIns);
+
+};
 
 dinnerCard();
 
