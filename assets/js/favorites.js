5830e6384977a3a5a643b3fd576f72ac4ea88751
soulslurpee <charles.d.thompson87@gmail.com>
2022-03-23 16:54:27 -0400

    pulled main

---
+++ assets/js/favorites.js
@@ -0,0 +1,15 @@
+var drinkInfo = [{"id": "0", "name":"drink", "sum":"here are some words", "img":"http://"}, 
+                {"id": "1", "name":"otherDrink", "sum":"here are some different words", "img":"https://"}];
+
+var dinnerInfo = [{"id": "0", "name":"dinner", "sum":"here are some words", "img":"http://"}, 
+                {"id": "1", "name":"otherDinner", "sum":"here are some different words", "img":"https://"}];
+
+var dummyDataPush = function() {
+    localStorage.setItem("drinkInfo", JSON.stringify(drinkInfo));
+    localStorage.setItem("dinnerInfo", JSON.stringify(drinkInfo));
+};
+
+dummyDataPush();
+
+console.log(JSON.parse(localStorage.getItem("drinkInfo")));
+
