63b4e577a2f1af5e9c229d29562e207315f62320
soulslurpee <charles.d.thompson87@gmail.com>
2022-03-24 18:36:56 -0400

    moved favorites.html into an assets folder

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -9,19 +9,30 @@ function allStorage () {
     for (var i = 0; i<localStorage.length; i++) {
 
         var favCardEl = document.createElement("div");
-        favSectionEl.appendChild(favCardEl);
-        
-            var cardTitleEl = document.createElement("h5");
-            favCardEl.appendChild(cardTitleEl);
-            var cardBodyEl = document.createElement("div")
-            favCardEl.appendChild(cardBodyEl);
+            favSectionEl.appendChild(favCardEl);
+            gitEl.setAttribute("id","fav"+[i]);
+            favCardEl.setAttribute("class","card col-5 m-2");
 
-                var favCardEl = document.createElement("img");
-                favSectionEl.appendChild(cardBodyEl);               
-                var favCardEl = document.createElement("p");
-                favSectionEl.appendChild(cardBodyEl);
-                var favCardEl = document.createElement("a");
-                favSectionEl.appendChild(cardBodyEl);
+            var cardTitleEl = document.createElement("h5");
+                favCardEl.appendChild(cardTitleEl);
+                cardTitleEl.setAttribute("class", "card-header");
+                cardTitleEl.innerText(archive[i].title);
+
+            var cardBodyEl = document.createElement("div");
+                favCardEl.appendChild(cardBodyEl);
+                cardBodyEl.setAttribute("class","card-body");
+
+                var cardImgEl = document.createElement("img");
+                    favSectionEl.appendChild(cardImgEl);    
+                    
+                var cardTextEl = document.createElement("p");
+                    favSectionEl.appendChild(cardTextEl);
+                    fav
+
+                var cardDeleteEl = document.createElement("a");
+                    favSectionEl.appendChild(cardDeleteEl);
+                    cardDeleteEl.setAttribute("class","btn btn-primary");
+                    cardDeleteEl.innerText("Unfavorite :(")
 
     };
 };
